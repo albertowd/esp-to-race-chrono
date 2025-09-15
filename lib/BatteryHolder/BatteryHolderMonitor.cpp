9ee3e0bab8da67bc84889bc78dacb12b942896e6
Alberto Wollmann Dietrich <alberto+git@albertowd.dev>
2025-09-15 16:03:00 -0300

    fix: format

--- lib/BatteryHolder/BatteryHolderMonitor.cpp
+++ lib/BatteryHolder/BatteryHolderMonitor.cpp
@@ -2,21 +2,25 @@
 
 #include "BatteryHolderMonitor.hpp"
 
-BatteryHolder::Monitor::Monitor(uint8_t batteryPin): batteryPin(batteryPin), lastValue(0U), lastVoltage(0.0f) {}
+BatteryHolder::Monitor::Monitor(uint8_t batteryPin) : batteryPin(batteryPin), lastValue(0U), lastVoltage(0.0f) {}
 
-float BatteryHolder::Monitor::getVoltage() const {
+float BatteryHolder::Monitor::getVoltage() const
+{
   return this->lastVoltage;
 }
 
-uint16_t BatteryHolder::Monitor::getValue() const {
+uint16_t BatteryHolder::Monitor::getValue() const
+{
   return this->lastValue;
 }
 
-void BatteryHolder::Monitor::setup() const {
+void BatteryHolder::Monitor::setup() const
+{
   pinMode(this->batteryPin, INPUT);
 }
 
-void BatteryHolder::Monitor::update(bool debug) {
+void BatteryHolder::Monitor::update(bool debug)
+{
   this->lastValue = analogRead(this->batteryPin);
 
   // Convert ADC value to voltage (adjust these values based on your board and battery)
@@ -24,7 +28,8 @@ void BatteryHolder::Monitor::update(bool debug) {
   // or voltage divider to get an accurate battery voltage reading.
   this->lastVoltage = this->lastValue * (5.0 / 4095.0); // For ESP32, 4095 is the max ADC value
 
-  if (debug) {
+  if (debug)
+  {
     Serial.print("ADC Value: ");
     Serial.print(this->lastValue);
     Serial.print(" | Battery Voltage: ");
